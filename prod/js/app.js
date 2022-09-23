1ca79721c509939035c94aae1d3ad0deada51ce1
dronovs <od270891dss@gmail.com>
2022-09-23 17:46:58 +0300

    update

--- prod/js/app.js
+++ prod/js/app.js
@@ -7,35 +7,74 @@ let lightThemeContainers = document.querySelectorAll('.light-theme');
 
 const ukrButton = document.querySelector('.ukr-lang-btn');
 const engButton = document.querySelector('.eng-lang-btn');
-const langButtons = document.querySelector('.lang-btns-wrapper');
 const ukrainianWrapper = document.querySelector('.ukrainian-wrapper');
 const englishWrapper = document.querySelector('.english-wrapper');
 
 const themeTumbler = document.querySelector('.theme-tumbler');
 const themeTumblerCircle = document.querySelector('.theme-tumbler-circle');
-console.log(themeTumblerCircle);
 
-function changeLanguage () {
+function headerEventListener () {
     if (event.target === engButton) {
+        event.stopPropagation();
         englishWrapper.classList.remove('unchecked-language');
         ukrainianWrapper.classList.add('unchecked-language');
-        console.log(ukrainianWrapper.classList);
-    }
-    if (event.target === ukrButton) {
+        localStorage.setItem('lang', 'eng');
+    } else if (event.target === ukrButton) {
+        event.stopPropagation();
         englishWrapper.classList.add('unchecked-language');
         ukrainianWrapper.classList.remove('unchecked-language');
+        localStorage.setItem('lang', 'ua');
+    } else if (event.target === themeTumbler || event.target === themeTumblerCircle) {
+        event.stopPropagation();
+        themeTumbler.classList.toggle('theme-tumbler-dark');
+        themeTumblerCircle.classList.toggle('theme-tumbler-circle-dark');
+        lightThemeContainers.forEach(el => {
+            el.classList.toggle('dark-theme');
+        })
+        header.classList.toggle('header-dark-theme');
+        body.classList.toggle('body-dark-theme');
+        if (localStorage.getItem('theme') === null) {
+            localStorage.setItem('theme', 'dark');
+        } else {
+            localStorage.removeItem('theme');
+        }
     }
 }
 
-function changeTheme () {
-    event.target.style.justifyContent = 'end';
+// function langLoader () {
+//     if (localStorage.getItem('lang') === 'eng') {
+//         englishWrapper.classList.remove('unchecked-language');
+//         ukrainianWrapper.classList.add('unchecked-language');
+//     } else if (localStorage.getItem('lang') === 'ua') {
+//         englishWrapper.classList.add('unchecked-language');
+//         ukrainianWrapper.classList.remove('unchecked-language');
+//     }
+//     if (localStorage.getItem('theme') === 'dark') {
+//         themeTumbler.classList.toggle('theme-tumbler-dark');
+//         themeTumblerCircle.classList.toggle('theme-tumbler-circle-dark');
+//         lightThemeContainers.forEach(el => {
+//             el.classList.toggle('dark-theme');
+//         })
+//         header.classList.toggle('header-dark-theme');
+//         body.classList.toggle('body-dark-theme');
+//     }
+// }
 
-    lightThemeContainers.forEach(el => {
-        el.classList.toggle('dark-theme');
-    })
-    header.classList.toggle('header-dark-theme');
-    body.classList.toggle('body-dark-theme');
+function langLoader () {
+    if (localStorage.getItem('lang') === 'eng') {
+        englishWrapper.classList.remove('unchecked-language');
+        ukrainianWrapper.classList.add('unchecked-language');
+    }
+    if (localStorage.getItem('theme') === 'dark') {
+        themeTumbler.classList.toggle('theme-tumbler-dark');
+        themeTumblerCircle.classList.toggle('theme-tumbler-circle-dark');
+        lightThemeContainers.forEach(el => {
+            el.classList.toggle('dark-theme');
+        })
+        header.classList.toggle('header-dark-theme');
+        body.classList.toggle('body-dark-theme');
+    }
 }
 
-themeTumbler.addEventListener('click', changeTheme);
-langButtons.addEventListener('click', changeLanguage);
+document.addEventListener('DOMContentLoaded', langLoader);
+header.addEventListener('click', headerEventListener);
